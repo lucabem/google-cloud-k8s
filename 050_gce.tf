059ad05633159bee50e39aa64b1e80fe6e130189
luis <luis.cabezon@bluetab.net>
2023-03-25 17:22:38 +0100

    chore: added new version

--- 050_gce.tf
+++ 050_gce.tf
@@ -71,42 +71,21 @@ resource "kubernetes_deployment" "deployment_app_web" {
 ################################################################################
 # SERVICE
 ################################################################################
-# resource "kubernetes_service" "service_app_web" {
-#   metadata {
-#     name      = "nuwe-app"
-#     namespace = kubernetes_namespace.namespace_app_web.metadata[0].name
-#     labels    = local.tags
-#   }
-#   spec {
-#     selector = {
-#       app = kubernetes_deployment.deployment_app_web.spec[0].template[0].metadata[0].labels.app
-#     }
-#     type = "LoadBalancer"
-#     port {
-#       port        = 80
-#       target_port = 3000
-#     }
-#   }
-# }
-
-data "kubectl_path_documents" "service_app_web" {
-  pattern = "./manifests/web_service.yaml"
-  vars = {
-    name         = "nuwe-service-app"
-    namespace    = kubernetes_namespace.namespace_app_web.metadata[0].name
-    app          = kubernetes_deployment.deployment_app_web.spec[0].template[0].metadata[0].labels.app
-    port         = 80
-    target_port  = 3000
-    type_service = "LoadBalancer"
+resource "kubernetes_service" "service_app_web" {
+  metadata {
+    name      = "nuwe-app"
+    namespace = kubernetes_namespace.namespace_app_web.metadata[0].name
+    labels    = local.tags
+  }
+  spec {
+    selector = {
+      app = kubernetes_deployment.deployment_app_web.spec[0].template[0].metadata[0].labels.app
+    }
+    type = "LoadBalancer"
+    port {
+      port        = 80
+      target_port = 3000
+    }
   }
 }
 
-
-resource "kubectl_manifest" "service_app_web" {
-  count            = length(data.kubectl_path_documents.service_app_web.documents)
-  yaml_body        = element(data.kubectl_path_documents.service_app_web.documents, count.index)
-  force_new        = true
-  wait             = true
-  wait_for_rollout = true
-
-}
